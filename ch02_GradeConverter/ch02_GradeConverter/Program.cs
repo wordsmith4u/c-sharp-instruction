c0b72f1be1929c7405cc44db1166bbc7400ccebb
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-01 12:47:20 -0500

    new commits

---
+++ ch02_GradeConverter/ch02_GradeConverter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ch02_GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the letter grade converter!");
+
+            // while loop - 'continue' loop, similar to Java projects
+            String choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.Write("Enter numerical grade: ");
+                int numericalGrade = int.Parse(Console.ReadLine());
+                String letterGrade = "";
+                if (numericalGrade >= 88)
+                {
+                    letterGrade = "A";
+                }
+                else if (numericalGrade >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if (numericalGrade >= 67)
+                {
+                    letterGrade = "C";
+                }
+                else if (numericalGrade >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+                
+                Console.Write("Letter Grade: " + letterGrade);
+                Console.WriteLine();
+
+                Console.Write("Continue? ");
+                choice = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
---
+++ ch02_RectangleCaculator/ch02_RectangleCaculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ch02_RectangleCaculator
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the rectangle calculator!");
+
+            String choice = "y";
+            while (choice.Equals("y"))
+            {
+                int l, w, area, perimeter;
+                Console.Write("Length : ");
+                l = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Width : ");
+                w = Convert.ToInt32(Console.ReadLine());
+                area = l * w;
+                perimeter = 2 * (l + w);
+                Console.WriteLine("Area of Rectangle : " + area);
+                Console.WriteLine("Perimeter of Rectangle : " + perimeter);
+
+                Console.Write("Continue? ");
+                choice = Console.ReadLine();
+
+            }
+            Console.WriteLine("Bye!");
+        }
+    }
+}
