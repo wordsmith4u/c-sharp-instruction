bb324df89ae4fc1e8c5be226c43e12c09aef09a2
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-02 21:21:40 -0500

    DoubleConversion

---
+++ ch03_DoubleConversion/ch03_DoubleConversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ch03_double_convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert a double!");
+            Console.Write("Enter a decimal value:  ");
+            String dStr = Console.ReadLine();
+            double d1 = Double.Parse(dStr);
+            double d2 = Convert.ToDouble(dStr);
+        }
+    }
+}
