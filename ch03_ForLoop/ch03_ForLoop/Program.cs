cb91924ae1eaa8b00f76b8af09282384c34d56d8
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-02 21:22:38 -0500

    For Loops

---
+++ ch03_ForLoop/ch03_ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ch03_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop Example");
+            // index for loop that will print "Hello" n times
+            // n is an int entered by the user
+
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Hello! " + i);
+
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
