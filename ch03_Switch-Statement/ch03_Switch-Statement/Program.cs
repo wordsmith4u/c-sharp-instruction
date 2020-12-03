b8344a032d81b68ecaf9dcbba44403209ac9273a
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-03 17:59:07 -0500

    Switch Statement

---
+++ ch03_Switch-Statement/ch03_Switch-Statement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ch03_Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch Statement Demo!");
+            // prompt user for a # between 1 and 7
+            // convert that # to a day of the week (Mon - Sun)
+
+            Console.Write("Enter a number between 1 and 7: ");
+            int n = Int32.Parse(Console.ReadLine());
+            String dow = "";
+            switch (n){
+                case 1:
+                    dow = "Monday";
+                    break;
+                case 2:
+                    dow = "Tuesday";
+                    break;
+                case 3:
+                    dow = "Wednesday";
+                    break;
+                case 4:
+                    dow = "Thursday";
+                    break;
+                case 5:
+                    dow = "Friday";
+                    break;
+                case 6:
+                    dow = "Saturday";
+                    break;
+                case 7:
+                    dow = "Sunday";
+                    break;
+                default:
+                    dow = "No day of the week";
+                    break;
+            }
+
+            Console.WriteLine("Day of week is'" + dow + "'.");
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
