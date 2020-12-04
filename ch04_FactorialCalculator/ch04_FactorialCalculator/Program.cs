8dc2cede971b4b1cd86054b1b9850119df606348
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-04 17:19:46 -0500

    Factorial Calculator

---
+++ ch04_FactorialCalculator/ch04_FactorialCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+public class FactorialExample
+{
+    public static void Main(string[] args) {
+        Console.WriteLine("Welcome to the factorial calculator app!");
+        Console.WriteLine();
+
+        String choice = "y";
+        while (choice.Equals("y"))
+        {
+            int i, fact = 1, number;
+        Console.Write("Enter an integer that's greater than 0 and less than 10: ");
+        number = int.Parse(Console.ReadLine());
+        for (i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+        Console.Write("The Factorial of " + number + " is: " + fact);
+            Console.WriteLine();
+        Console.Write("Continue? ");
+        choice = Console.ReadLine();
+    }
+
+
+    Console.WriteLine("Bye!");
+        }
+    }
+
